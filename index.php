c3b5a6efb80ef99e3bd40aafca9962e0b5498df3
Mikael <mikaeladami2018@gmail.com>
2024-04-13 19:41:41 -0300

    message 3

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Natu Ilhéus</title>
-    <link rel="shortcut icon" href="images/logo.jpg">
+    <link rel="shortcut icon" href="images/logo.png">
 </head>
 <body>
-    
+    <img src="images/logo.svg" alt="eae">
 </body>
 </html>
