c18a686475d3725db39ed455680933098ca81517
Mikael <mikaeladami2018@gmail.com>
2024-04-20 23:10:56 -0300

    all

--- index.php
+++ index.php
@@ -3,11 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Natu Ilhéus</title>
-    <link rel="shortcut icon" href="images/logo.png">
-    <link rel="icon" href="/path/to/favicon.ico">
+    <link rel="shortcut icon" href="images/iconlogo.jpg">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-
+<header>
+    <div class="information">
+        <div>
+            <span class="material-icons">call</span>
+            <span style="letter-spacing: 1px;" >+(73)999984706</span>
+        </div>
+        <div>
+            <span class="material-icons">mail</span>
+            <span style="letter-spacing: 1px;" >NATUILHEUS@GMAIL.COM</span>
+        </div>
+        <div>
+            <span style="letter-spacing: 1px;">ENTREGAMOS EM TODO PAÍS & FRETE GRÁTIS</span>
+        </div>
+    </div>
+    <nav class="header">
+        <a href="index.php" style="letter-spacing: 1.5px;">NATU ILHÉUS</a>
+        <ul class="paginas">
+            <li>
+                <a href="about.php">Sobre</a>
+            </li>
+            <li>
+                <a href="#products">Produtos</a>
+            </li>
+            <li>
+                <a href="contact.php">Contato</a>
+            </li>
+            <li>
+                <span class="material-icons"><a href="cart.php">shopping_cart</a></span>
+            </li>
+        </ul>
+    </nav>
+</header>
+<main>
+<div class="principal"></div>
+</main>
 </body>
 </html>
